2ea6ed63cb2893cd5bdf2f6917a650af56d014ec
dungnvf-0423 <139527315+dungnvf-0423@users.noreply.github.com>
2023-09-05 16:38:42 +0700

    add picture product

--- middleware/uploadImage.js
+++ middleware/uploadImage.js
@@ -1,16 +1,16 @@
 const multer = require("multer");
 
-function uploadImage(kindImage){
+function uploadImage(kindImage, imageNames){
     const storage = multer.diskStorage({
         destination: (request, file, cb) => {
             cb(null, process.cwd() + "/public/images/" + kindImage + "/"); // Thư mục để lưu trữ tệp tin tải lên.
         },
         filename: (request, file, cb) => {
-            cb(null, file.originalname); // Tạo tên tệp tin duy nhất.
+            const imageName = Date.now() + file.originalname.split(' ').join('')
+            imageNames.push(imageName)
+            cb(null, imageName); // Tạo tên tệp tin duy nhất.
         },
     });
-
-
     return multer({ storage });
 }
 
--- seeders/20230809073744-productImage-seeder.js
+++ seeders/20230809073744-productImage-seeder.js
@@ -9,243 +9,243 @@ module.exports = {
                 // Clothes
                 {
                     product_id: 1,
-                    image: "http://localhost:8000/images/product_images/product1_1.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_1.png",
                 },
                 {
                     product_id: 1,
-                    image: "http://localhost:8000/images/product_images/product1_2.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_2.png",
                 },
                 {
                     product_id: 1,
-                    image: "http://localhost:8000/images/product_images/product1_3.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_3.png",
                 },
                 {
                     product_id: 1,
-                    image: "http://localhost:8000/images/product_images/product1_4.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_4.png",
                 },
                 {
                     product_id: 1,
-                    image: "http://localhost:8000/images/product_images/product1_5.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_5.png",
                 },
                 {
                     product_id: 1,
-                    image: "http://localhost:8000/images/product_images/product1_6.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_6.png",
                 },
                 {
                     product_id: 1,
-                    image: "http://localhost:8000/images/product_images/product1_7.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_7.png",
                 },
                 {
                     product_id: 2,
-                    image: "http://localhost:8000/images/product_images/product2_1.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_1.png",
                 },
                 {
                     product_id: 2,
-                    image: "http://localhost:8000/images/product_images/product2_2.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_2.png",
                 },
                 {
                     product_id: 2,
-                    image: "http://localhost:8000/images/product_images/product2_3.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_3.png",
                 },
                 {
                     product_id: 2,
-                    image: "http://localhost:8000/images/product_images/product2_4.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_4.png",
                 },
                 {
                     product_id: 2,
-                    image: "http://localhost:8000/images/product_images/product2_5.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_5.png",
                 },
                 {
                     product_id: 2,
-                    image: "http://localhost:8000/images/product_images/product2_6.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_6.png",
                 },
                 {
                     product_id: 3,
-                    image: "http://localhost:8000/images/product_images/product3_1.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_1.png",
                 },
                 {
                     product_id: 3,
-                    image: "http://localhost:8000/images/product_images/product3_2.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_2.png",
                 },
                 {
                     product_id: 3,
-                    image: "http://localhost:8000/images/product_images/product3_3.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_3.png",
                 },
                 {
                     product_id: 3,
-                    image: "http://localhost:8000/images/product_images/product3_4.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_4.png",
                 },
                 {
                     product_id: 3,
-                    image: "http://localhost:8000/images/product_images/product3_5.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_5.png",
                 },
                 {
                     product_id: 3,
-                    image: "http://localhost:8000/images/product_images/product3_6.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_6.png",
                 },
                 {
                     product_id: 3,
-                    image: "http://localhost:8000/images/product_images/product3_7.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_7.png",
                 },
                 {
                     product_id: 4,
-                    image: "http://localhost:8000/images/product_images/product1_1.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_1.png",
                 },
                 {
                     product_id: 4,
-                    image: "http://localhost:8000/images/product_images/product1_2.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_2.png",
                 },
                 {
                     product_id: 4,
-                    image: "http://localhost:8000/images/product_images/product1_3.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_3.png",
                 },
                 {
                     product_id: 4,
-                    image: "http://localhost:8000/images/product_images/product1_4.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_4.png",
                 },
                 {
                     product_id: 4,
-                    image: "http://localhost:8000/images/product_images/product1_5.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_5.png",
                 },
                 {
                     product_id: 4,
-                    image: "http://localhost:8000/images/product_images/product1_6.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_6.png",
                 },
                 {
                     product_id: 4,
-                    image: "http://localhost:8000/images/product_images/product1_7.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_7.png",
                 },
                 {
                     product_id: 5,
-                    image: "http://localhost:8000/images/product_images/product2_1.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_1.png",
                 },
                 {
                     product_id: 5,
-                    image: "http://localhost:8000/images/product_images/product2_2.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_2.png",
                 },
                 {
                     product_id: 5,
-                    image: "http://localhost:8000/images/product_images/product2_3.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_3.png",
                 },
                 {
                     product_id: 5,
-                    image: "http://localhost:8000/images/product_images/product2_4.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_4.png",
                 },
                 {
                     product_id: 5,
-                    image: "http://localhost:8000/images/product_images/product2_5.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_5.png",
                 },
                 {
                     product_id: 5,
-                    image: "http://localhost:8000/images/product_images/product2_6.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_6.png",
                 },
                 {
                     product_id: 6,
-                    image: "http://localhost:8000/images/product_images/product3_1.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_1.png",
                 },
                 {
                     product_id: 6,
-                    image: "http://localhost:8000/images/product_images/product3_2.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_2.png",
                 },
                 {
                     product_id: 6,
-                    image: "http://localhost:8000/images/product_images/product3_3.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_3.png",
                 },
                 {
                     product_id: 6,
-                    image: "http://localhost:8000/images/product_images/product3_4.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_4.png",
                 },
                 {
                     product_id: 6,
-                    image: "http://localhost:8000/images/product_images/product3_5.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_5.png",
                 },
                 {
                     product_id: 6,
-                    image: "http://localhost:8000/images/product_images/product3_6.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_6.png",
                 },
                 {
                     product_id: 6,
-                    image: "http://localhost:8000/images/product_images/product3_7.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_7.png",
                 },
                 {
                     product_id: 7,
-                    image: "http://localhost:8000/images/product_images/product3_1.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_1.png",
                 },
                 {
                     product_id: 7,
-                    image: "http://localhost:8000/images/product_images/product3_2.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_2.png",
                 },
                 {
                     product_id: 7,
-                    image: "http://localhost:8000/images/product_images/product3_3.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_3.png",
                 },
                 {
                     product_id: 7,
-                    image: "http://localhost:8000/images/product_images/product3_4.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_4.png",
                 },
                 {
                     product_id: 7,
-                    image: "http://localhost:8000/images/product_images/product3_5.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_5.png",
                 },
                 {
                     product_id: 7,
-                    image: "http://localhost:8000/images/product_images/product3_6.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_6.png",
                 },
                 {
                     product_id: 7,
-                    image: "http://localhost:8000/images/product_images/product3_7.png",
+                    image: process.env.WEB_URL + "/images/product_images/product3_7.png",
                 },
                 {
                     product_id: 8,
-                    image: "http://localhost:8000/images/product_images/product1_1.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_1.png",
                 },
                 {
                     product_id: 8,
-                    image: "http://localhost:8000/images/product_images/product1_2.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_2.png",
                 },
                 {
                     product_id: 8,
-                    image: "http://localhost:8000/images/product_images/product1_3.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_3.png",
                 },
                 {
                     product_id: 8,
-                    image: "http://localhost:8000/images/product_images/product1_4.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_4.png",
                 },
                 {
                     product_id: 8,
-                    image: "http://localhost:8000/images/product_images/product1_5.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_5.png",
                 },
                 {
                     product_id: 8,
-                    image: "http://localhost:8000/images/product_images/product1_6.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_6.png",
                 },
                 {
                     product_id: 8,
-                    image: "http://localhost:8000/images/product_images/product1_7.png",
+                    image: process.env.WEB_URL + "/images/product_images/product1_7.png",
                 },
                 {
                     product_id: 9,
-                    image: "http://localhost:8000/images/product_images/product2_1.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_1.png",
                 },
                 {
                     product_id: 9,
-                    image: "http://localhost:8000/images/product_images/product2_2.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_2.png",
                 },
                 {
                     product_id: 9,
-                    image: "http://localhost:8000/images/product_images/product2_3.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_3.png",
                 },
                 {
                     product_id: 9,
-                    image: "http://localhost:8000/images/product_images/product2_4.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_4.png",
                 },
                 {
                     product_id: 9,
-                    image: "http://localhost:8000/images/product_images/product2_5.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_5.png",
                 },
                 {
                     product_id: 9,
-                    image: "http://localhost:8000/images/product_images/product2_6.png",
+                    image: process.env.WEB_URL + "/images/product_images/product2_6.png",
                 },
 
                 {
