814e71311d65dc633ee1a1d281f436ae6e4d84df
Nguyen Duc Phu <phunguyenduc99@gmail.com>
2023-09-06 14:54:51 +0700

    commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const userController = require("../controllers/api/user.controller");
 const checkRoleMiddleware = require("../middleware/check-role");
-const {uploadImage} = require("../middleware/uploadImage");
+const { uploadImage } = require("../middleware/uploadImage");
 
 const router = express.Router();
 
 router.get(
-    "/profile",
-    checkRoleMiddleware.checkRoleUser,
-    userController.getUserById
+  "/profile",
+  checkRoleMiddleware.checkRoleUser,
+  userController.getUserById
 );
 
 var imageNames = [];
 
 router.patch(
-    "/update-ava",
-    // checkRoleMiddleware.checkRoleUser,
-    uploadImage("avatars", imageNames).array("images", 1),
-    (req, res) => {
-        req.body.image = imageNames[0];
-        userController.updateAva(req,res);
-    }
+  "/update-ava",
+  checkRoleMiddleware.checkRoleUser,
+  uploadImage("avatars", imageNames).array("images", 1),
+  (req, res) => {
+    req.body.image = imageNames[0];
+    userController.updateAva(req, res);
+  }
 );
 
 router.patch(
