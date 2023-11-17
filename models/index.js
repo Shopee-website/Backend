cb63e837db76e2c8b82beeaa095416783f5c8454
DungNguyen1702 <vandung17022003@gmail.com>
2023-11-17 22:35:00 +0700

    add library to deploy

--- models/index.js
+++ models/index.js
@@ -7,41 +7,41 @@ require("dotenv").config();
 
 // Database trên vercel
 
-// const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-//     host: process.env.POSTGRES_HOST,
-//     dialect: process.env.POSTGRES_DIALECT ,
-//     operatorsAliases: false,
-//     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-//     pool: {
-//         max: parseInt(process.env.POOL_MAX),
-//         min: parseInt(process.env.POOL_MIN),
-//         acquire: parseInt(process.env.POOL_ACQUIRE),
-//         idle: parseInt(process.env.POOL_IDLE),
-//     },
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false, // Chỉ sử dụng trong môi trường phát triển hoặc kiểm tra, không nên sử dụng trong môi trường sản xuất.
-//         },
-//     },
-// });
+const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.POSTGRES_DIALECT ,
+    operatorsAliases: false,
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    pool: {
+        max: parseInt(process.env.POOL_MAX),
+        min: parseInt(process.env.POOL_MIN),
+        acquire: parseInt(process.env.POOL_ACQUIRE),
+        idle: parseInt(process.env.POOL_IDLE),
+    },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Chỉ sử dụng trong môi trường phát triển hoặc kiểm tra, không nên sử dụng trong môi trường sản xuất.
+        },
+    },
+});
 
 // Database trên local
 
-const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD,
-    {
-        host: process.env.POSTGRES_HOST,
-        dialect: process.env.POSTGRES_DIALECT,
-        operatorsAliases: false,
+// const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD,
+//     {
+//         host: process.env.POSTGRES_HOST,
+//         dialect: process.env.POSTGRES_DIALECT,
+//         operatorsAliases: false,
 
-        pool: {
-            max: parseInt(process.env.POOL_MAX),
-            min: parseInt(process.env.POOL_MIN),
-            acquire: parseInt(process.env.POOL_ACQUIRE),
-            idle: parseInt(process.env.POOL_IDLE),
-        },
-    }
-);
+//         pool: {
+//             max: parseInt(process.env.POOL_MAX),
+//             min: parseInt(process.env.POOL_MIN),
+//             acquire: parseInt(process.env.POOL_ACQUIRE),
+//             idle: parseInt(process.env.POOL_IDLE),
+//         },
+//     }
+// );
 
 fs.readdirSync(__dirname)
     .filter((file) => {
